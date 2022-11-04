3f6ae806a7854ff63d7d2a6d42bcbac55905c345
lakshman-uptycs <llukalapu@uptycs.com>
2022-11-04 09:39:26 +0530

    adding variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "resource_name" {
+  description = "Used to naming the new resources"
+  type        = string
+  default     = "uptycs-cloudquery-integration"
+}
+variable "set_org_level_permissions" {
+  description = "The flag to choose permissions to be attached to the application at tenant level or subscription level"
+  type    = bool
+  default     = true
+}
+variable "parent_management_group_name" {
+  description = "ID of the Root Management Group"
+  type   = string
+}

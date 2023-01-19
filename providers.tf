20341497d3b6e1efe4ce27e645a28034e907700f
diyor-uptycs <99381687+diyor-uptycs@users.noreply.github.com>
2023-01-19 12:45:00 +0500

    cross-tenant access changes

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.29.1"
+      version = ">= 3.39.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.3.0"
+      version = ">= 2.32.0"
     }
   }
 }
